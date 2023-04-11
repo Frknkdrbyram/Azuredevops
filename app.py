dfbb91cebf4f00bddecc5688cd133c197ece8720
Frknkdrbyram <furkankadirw@gmail.com>
2023-04-11 22:58:23 +0000

    update

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
 import logging
@@ -21,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home(Updated)</h3>"
+    html = "<h3>Sklearn Prediction Home(CI/CD  Pipeline Updated)-2</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
@@ -47,53 +46,3 @@ def predict():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
-=======
-from flask import Flask, request, jsonify
-from flask.logging import create_logger
-import logging
-
-import pandas as pd
-import joblib
-from sklearn.preprocessing import StandardScaler
-
-app = Flask(__name__)
-LOG = create_logger(app)
-LOG.setLevel(logging.INFO)
-
-def scale(payload):
-    """Scales Payload"""
-
-    LOG.info("Scaling Payload: %s payload")
-    scaler = StandardScaler().fit(payload)
-    scaled_adhoc_predict = scaler.transform(payload)
-    return scaled_adhoc_predict
-
-@app.route("/")
-def home():
-    html = "<h3>Sklearn Prediction Home(CI/CD Pipeline Updated)</h3>"
-    return html.format(format)
-
-# TO DO:  Log out the prediction value
-@app.route("/predict", methods=['POST'])
-def predict():
-    # Performs an sklearn prediction
-    try:
-        # Load pretrained model as clf. Try any one model. 
-        # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
-        # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
-        clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
-    except:
-        LOG.info("JSON payload: %s json_payload")
-        return "Model not loaded"
-
-    json_payload = request.json
-    LOG.info("JSON payload: %s json_payload")
-    inference_payload = pd.DataFrame(json_payload)
-    LOG.info("inference payload DataFrame: %s inference_payload")
-    scaled_payload = scale(inference_payload)
-    prediction = list(clf.predict(scaled_payload))
-    return jsonify({'prediction': prediction})
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
->>>>>>> a11261cd3dfbf11f2432f4585d389919c1999e47
